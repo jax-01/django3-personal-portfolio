dd2e0cd276b2afacb4ca71bda2aeccdba5286488
johnalbert <jrcastillo6@up.edu.ph>
2022-07-28 23:31:33 +0800

    Create initial structure in 'models.py' for database

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,5 +1,12 @@
+from operator import mod
+from pyexpat import model
 from turtle import title
 from django.db import models
 
 # Create your models here.
 
+class Project(models.Model):
+  title = models.CharField(max_length=100)
+  description = models.CharField(max_length=250)
+  image = models.ImageField(upload_to='portfolio/images/')
+  url = models.URLField(blank=True)
